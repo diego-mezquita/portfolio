d34a7c27f821590f8abf2e5d90cd507a35818bf0
diego-mezquita <diegomezquitafreijoo@gmail.com>
2018-01-10 00:46:33 +0100

    First approach to the contact me sidebar with its animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!');
+
+
+$(document).ready(function() {
+  $( '.js-contact-button' ).click(showContactSidebar);  
+});
+
+function showContactSidebar() {
+  $( '.js-contact' ).toggleClass('switcher');
+}
